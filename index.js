bd1d41390d1870ebb2beee5b370ac3d568b4ba0d
SimonFolkerts <simfolkerts@gmail.com>
2023-03-03 15:56:52 +1300

    add comments and working GET all and GET by ID routes

--- index.js
+++ index.js
@@ -1,31 +1,79 @@
-// import packages
+// INITIAL SETUP
+
+// import packages that make everytihing work
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// import Models
+// import Models (User, Article, etc). These are JavaScript representations of the collections of data
 const User = require("./models/user");
 
+// create a server instance
 const app = express();
+// ----------------------------------------------------------------
 
-// routes go here
-app.get("/users/:username", async (req, res) => {
-  const userArray = await User.find({});
+// ROUTES
+
+// get all users
+/* note that since we are working with an external database, the finding of the data will be asynchronous, because it takes 
+some amount of time for the database to receive the query and respond. We will need to tell our function to wait while this
+happens, so we must make it asynchronous, hence we put `async` in front */
+app.get("/users/", async (req, res) => {
+  /* here we can use User, mongoose's representation of the user data that is based on a Schema we created, to ask mongoose to go find users.
+  Each mongoose Model has a set of methods that they can use to communicate to MongoDB and interact with the collection
+  they represent. In this case, we are using .find(), which requires an object that it uses to specify what is to be 
+  found from the collection. Here, we are just leaving the filter object empty to say we want all items in the users collection */
+  // this returns an array of documents, represented by JavaScript objects. We can send this straight to the client as JSON
+  const userArray = await User.find({}); // <-- note that we need to `await` this as it takes some time to resolve
+
+  // to confirm we are getting results we can console.log here to see what was returned
   console.log(userArray);
+
+  // finally, we can send the data back to the client from which the get request was sent
   res.json(userArray);
 });
 
+// get users by id
+/* this route matches with any request that is a GET request to localhost:3000/users/* 
+In this case, * can be anything as it is a dynameic segment, and we will assume it is a user ID. Since we are using MongoDB, each user will have been
+assigned a unique id by the database system. If we send a reqeust up with one of these in the second segment of the url,
+then we can use that with `.findById()` to get it from the database */
+app.get("/users/:userId", async (req, res) => {
+  /* we can use .findById(), which requires an ID string as an argument 
+  .findById is a method all Models have that allows them to seek out a specific entry in the collection they represent.
+  If it finds a matching document in the database, it returns a JavaScript object (also called a document) that represents this data */
+  const requestedUser = await User.findById(req.params.userId);
+
+  // just checking here to see if we are actually getting anything back
+  console.log(requestedUser);
+
+  // send back the retreived user information object
+  res.json(requestedUser);
+});
+// ----------------------------------------------------------------
+
+// DATABASE CONNECTION
+
+/* Establish an initial connection with MongoDB using the connection string. If the initial connection fails (often
+  due to wrong credentials), then it will emit an error which we can handle with `.catch()`
+*/
 mongoose.connect(process.env.DB_STRING).catch((error) => {
-  console.log(error);
+  // callback function that runs if the error listener receives an error object
+  console.log(error); // <-- just console log the error for now
 });
 
+// we can also add listeners to the connection to MongoDB that monitor the connection for events
+// this listener will fire once the connection is established, and it then executes its callback function
 mongoose.connection.on("connected", () => {
+  // the callback function logs a success message
   console.log("connected to database");
+  // and then runs the code that makes the server start listening for HTTP request from clients
   app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
   });
 });
 
+// this listener fires if there is an error at any point while the connection is active, and logs it to the console
 mongoose.connection.on("error", (error) => {
   console.log(error);
 });
--- readme.md
+++ readme.md
@@ -8,3 +8,17 @@ PORT=3000
 DB_STRING=asdfasdfasdf
 
 then run `npx nodemon index.js` to start the server.
+
+## Fetching data
+
+This project currently contains an example of how to connect an express API to a database using Mongoose, rather than just using a JSON file. It also has two examples of how to use Mongoose to allow the express routes to retreive the data needed to fulfill a GET request for all users, and a GET request for a specific user by ID.
+
+### GET all
+
+If you send a GET request to the API using just the users endpoint `localhost:3000/users/` it will find a list of all users and return it to the client.
+
+### GET one by ID
+
+If you send a GET request to the API using the users endpoint with another segment after it, the API will attempt to use whatever is in that second segment as a MongoDB ID and will attempt to find a matching user and return that user to the client.
+
+This will be added to over the coming days.
