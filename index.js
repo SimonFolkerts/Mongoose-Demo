cf2bc4d58b63abc1d1747f71bee79411b6743a35
SimonFolkerts <simfolkerts@gmail.com>
2023-03-06 10:08:49 +1300

    add post, put

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const User = require("./models/user");
 
 // create a server instance
 const app = express();
+app.use(express.json());
 // ----------------------------------------------------------------
 
 // ROUTES
@@ -49,6 +50,37 @@ app.get("/users/:userId", async (req, res) => {
 
   // send back the retreived user information object
   res.json(requestedUser);
+});
+
+// create a user
+/* here the request will have incoming data attached to the body. Assuming this data matches the Schema,
+we can use it to create a new user document in the database using the .create() method. The create method
+will then return the newly created user from the databse which you can then send back to the client if you like */
+app.post("/users/", async (req, res) => {
+  // use the data to create a new user. NOTE the data must match the requirements of the Schema or else it will be rejected
+  const newUser = await User.create(req.body);
+
+  // can then either end the interaction with .end() or send back the new user like below
+  res.json(newUser);
+});
+
+// update user
+/* updating a user can be done in several ways, the way shown here is quite simple, but has three parts.
+first we need to find and retreive the user we want to update. Then we edit their document (it's an object so we
+can just use regular object notation using the membership operator '.'). Finally we save the edited user. Running save
+on an existing user will cause it to be updated rather than a new user being created */
+app.put("/users/:userId", async (req, res) => {
+  // get the user document to be edited from the database using findById()
+  const userToUpdate = await User.findById(req.params.userId);
+
+  // edit the user document (here we replace the user email address with the one from the request body)
+  userToUpdate.email = req.body.email;
+
+  // finally we save the edited user back to the database. This also returns the user, so we catch it in a variable
+  const updatedUser = await userToUpdate.save();
+
+  // and then we can send that variable back to the client if we like
+  res.json(updatedUser);
 });
 // ----------------------------------------------------------------
 
