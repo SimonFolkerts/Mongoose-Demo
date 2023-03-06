477f7747347c2332baaf5dbd07614bd7972b1a8b
SimonFolkerts <simfolkerts@gmail.com>
2023-03-07 10:07:24 +1300

    add validation function for email field in User Schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,21 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+
+    // add a custom validation function to the Schema
+    validate: {
+      // test function
+      validator: (value) => {
+        return /^\S+@\S+$/.test(value);
+      },
+      // fail message
+      message: "invalid email address",
+    },
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
